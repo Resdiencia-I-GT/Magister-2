3a794f6f2f3eadfbb75ed76c308568bb1afd7e98
HugoORamos <HugoOli.Ramos@gmail.com>
2023-10-30 15:26:26 -0300

    Update footer.tsx
    fiz as alteracoes que everton requisitou

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,56 +1,37 @@
+import React, { useState, useEffect } from "react";
+
 const Footer = () => {
+
+  const [campusInfo, setCampusInfo] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://magister-2.free.mockoapp.net/teste");
+        const info = await response.json();
+        setCampusInfo(info);
+      } catch (error) {
+        console.error("Erro ao buscar dados da API: ", error);
+      }
+    };
+
+    fetchData(); 
+
+  }, []); 
+
   return (
     <footer className="footer">
       <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <h3>Campus Aracaju-Centro</h3>
-            <p>R. Lagarto, 264</p>
-            <p>Bairro Centro</p>
-            <p>CEP: 49010-390</p>
-            <p>Fone: (79) 3218-2100</p>
-            <p>Fax: (79) 3218-2309</p>
-            <p>Aracaju/SE</p>
+        {campusInfo.map((campus, index) => (
+          <div className="row" key={index}>
+            <div className="col-md-6">
+              <h3>{campus.campus}</h3>
+              <p>{campus.endereco}</p>
+              <p>{campus.cidade}</p>
+              <p>Telefone: {campus.telefone}</p>
+            </div>
           </div>
-          <div className="col-md-6">
-            <h3>Campus Aracaju-Farolândia</h3>
-            <p>Av. Murilo Dantas, 300</p>
-            <p>Bairro Farolândia</p>
-            <p>CEP: 49032-490</p>
-            <p>Fone: (79) 3218-2100</p>
-            <p>Fax: (79) 3218-2200</p>
-            <p>Aracaju/SE</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <h3>Campus Estância</h3>
-            <p>Tv. Tenente Eloy, s/n</p>
-            <p>Bairro Alagoas</p>
-            <p>CEP: 49200-000</p>
-            <p>Fone: (79) 3522-3030</p>
-            <p>Estância/SE</p>
-          </div>
-          <div className="col-md-6">
-            <h3>Campus Itabaiana</h3>
-            <p>R. José Paulo Santana, 1254</p>
-            <p>Bairro Sítio Porto</p>
-            <p>CEP: 49500-000</p>
-            <p>Fone: (79) 3431-5050</p>
-            <p>Fax: (79) 3431-7070</p>
-            <p>Itabaiana/SE</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <h3>Campus Propriá</h3>
-            <p>Praça Santa Luzia, 105</p>
-            <p>Bairro Centro</p>
-            <p>CEP: 49900-000</p>
-            <p>Fone: (79) 3322-2774</p>
-            <p>Propriá/SE</p>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="bottom-bar">
         <div className="container">
